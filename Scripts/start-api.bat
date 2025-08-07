4eb5299eb2c33492e40475c72c56666b4b16dc48
Anton Urbanovich <antonurbanovic@gmail.com>
2025-08-07 11:54:12 +0300

    [Fix] Excluded swift image building until the issue with image size is resolved

--- Scripts/start-api.bat
+++ Scripts/start-api.bat
@@ -11,8 +11,8 @@ echo Building and loading csharp runner image
 docker build -t csharp-runner:local -f Runners/DotNetRunner/Dockerfile .
 minikube image load csharp-runner:local
 
-REM docker build -t swift-runner:local -f Runners/SwiftRunner/Dockerfile .
-REM minikube image load swift-runner:local
+::docker build -t swift-runner:local -f Runners/SwiftRunner/Dockerfile .
+::minikube image load swift-runner:local
 
 
 
