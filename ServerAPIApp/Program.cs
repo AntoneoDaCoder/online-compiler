276cbfd03396d82dc29bdfcab0b794580c2992c4
Anton Urbanovich <antonurbanovic@gmail.com>
2025-08-07 11:54:30 +0300

    [Fix] Added CORS

--- ServerAPIApp/Program.cs
+++ ServerAPIApp/Program.cs
@@ -7,6 +7,19 @@ builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200") 
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+
 builder.Services.BindLanguageConfigs(builder.Configuration);
 
 builder.Services.RegisterServices();
@@ -14,6 +27,8 @@ builder.Services.RegisterServices();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapControllers();
 
