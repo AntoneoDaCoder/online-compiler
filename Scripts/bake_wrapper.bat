029c627fc0808df6dfcfcedb4b1d53a02c92f27a
Anton Urbanovich <antonurbanovic@gmail.com>
2025-08-29 20:15:44 +0300

    [DevOps/Fix] Fixed misleading build progress output

--- Scripts/bake_wrapper.bat
+++ Scripts/bake_wrapper.bat
@@ -1,7 +1,8 @@
 @echo off
-
 cd ..
-docker buildx bake --file scripts/docker-bake.hcl --progress=plain 2>&1 | findstr /i "ERROR failed error"
+
+docker buildx bake --file scripts/docker-bake.hcl --progress=plain 2>&1 | ^
+findstr /i "error failed" | findstr /v "0 Error(s)" >nul
 
 if %errorlevel%==0 (
     echo [91mBUILD FAILED[0m
