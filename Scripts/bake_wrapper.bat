e791815241d7678a2be24850072c2c142634661e
Anton Urbanovich <antonurbanovic@gmail.com>
2025-08-29 11:36:08 +0300

    [DevOps/Refactoring] Moved image copying to Makefile

--- Scripts/bake_wrapper.bat
+++ Scripts/bake_wrapper.bat
@@ -9,16 +9,4 @@ if %errorlevel%==0 (
     echo [92mBUILD SUCCESS[0m
 )
 
-echo [93mLoading images into minikube...[0m
-minikube -p minikube image load api-server:local
-minikube -p minikube image load csharp-runner:local
-minikube -p minikube image load java-runner:local
-minikube -p minikube image load postgresql-runner:local
-minikube -p minikube image load nodejs-runner:local
-minikube -p minikube image load kotlin-runner:local
-minikube -p minikube image load typescript-runner:local
-:: minikube -p minikube image load swift-runner:local
-echo [92mAll images have been loaded[0m
-
-
 exit /b 0
