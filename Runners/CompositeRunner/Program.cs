563caa53cb2d6378f6a61b91ea79b72b0b7130be
Anton Urbanovich <antonurbanovic@gmail.com>
2025-09-10 11:44:40 +0300

    [BE] Implemented composite runner project

---
+++ Runners/CompositeRunner/Program.cs
@@ -0,0 +1,38 @@
+﻿using Runners.Shared;
+using Runners.Shared.Runners;
+
+class Runner
+{
+    public static async Task<int> Main()
+    {
+        using var cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (s, e) =>
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        var supportedRunners = new Dictionary<string, IRunner>
+        {
+            {"csharp", new DotNetRunner() },
+            {"java", new JavaCompositeRunner() },
+            {"kotlin",new KotlinCompositeRunner() },
+            {"nodejs", new NodeJsRunner() },
+            {"typescript", new TypeScriptRunner() },
+            {"mssql", new MsSqlRunner() },
+            {"postgresql",new PostgresqlRunner() }
+        };
+
+        using (var runner = new CompositeRunner(supportedRunners))
+        {
+            await runner.StartAsync(cts.Token);
+
+            await runner.ListenAsync(cts.Token);
+
+            await runner.StopAsync(cts.Token);
+        }
+
+        return 0;
+    }
+
+}
