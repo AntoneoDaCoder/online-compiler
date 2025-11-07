d28c5d1b050c24cce5993be3a6bc1b3bc61c2cf2
Anton Urbanovich <antonurbanovic@gmail.com>
2025-11-07 22:23:50 +0300

    [BE] Created submission entity

---
+++ ServerAPIApp.Domain/Entities/SubmissionEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerAPIApp.Domain.Entities
+{
+    public class SubmissionEntity : BaseEntity, ICreatable
+    {
+        public ProblemVersionEntity? ProblemVersion { get; set; }
+
+        //public User? Creator {get;set;}
+        public Guid ProblemVersionId { get; set; }
+        public Guid CreatorId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string Solution { get; set; } = "Sample solution";
+        public string Status { get; set; } = "Sample status";
+        public string SolutionLanguage { get; set; } = "Sample lamguage";
+    }
+}
