9038ebf7c07d8d4326e0b5f459612d7cbe622bb6
Anton Urbanovich <antonurbanovic@gmail.com>
2025-11-08 16:16:04 +0300

    [BE] Defined user repo interface

---
+++ ServerAPIApp.Contracts/Abstractions/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ServerAPIApp.Domain.Entities;
+
+namespace ServerAPIApp.Contracts.Abstractions
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity?> GetByIdAsync
+            (Guid userId,
+            CancellationToken cancellationToken = default);
+        Task<UserEntity?> GetByEmailAsync
+            (string email,
+            CancellationToken cancellationToken = default);
+        Task<IdentityResult> UpdateAsync
+            (UserEntity user,
+            CancellationToken cancellationToken = default);
+        Task<IdentityResult> CreateAsync
+            (UserEntity user,
+            CancellationToken cancellationToken = default);
+        Task<bool> CheckPasswordAsync
+            (UserEntity user,
+            string password,
+            CancellationToken cancellationToken = default);
+    }
+}
