0c7c240ab3d42f7e9fa40f30c2309105d2af1635
Anton Urbanovich <antonurbanovic@gmail.com>
2025-11-11 11:56:38 +0300

    [BE] Added Language & ProblemVersionLanguage entities

---
+++ ServerAPIApp.Domain/Entities/ProblemVersionLanguage.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerAPIApp.Domain.Entities
+{
+    public class ProblemVersionLanguagesEntity
+    {
+        public Guid VersionId { get; set; }
+        public ProblemVersionEntity? Version { get; set; }
+        public Guid LanguageId { get; set; }
+        public LanguageEntity? Language { get; set; }
+    }
+}
