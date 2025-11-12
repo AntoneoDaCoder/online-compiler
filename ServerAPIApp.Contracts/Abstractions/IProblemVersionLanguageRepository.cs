b3421b2870ef29dbd87b0d22ab6347be7bbe4d01
Anton Urbanovich <antonurbanovic@gmail.com>
2025-11-12 12:54:23 +0300

    [BE] Added repo for version languages & finished implementing db context

---
+++ ServerAPIApp.Contracts/Abstractions/IProblemVersionLanguageRepository.cs
@@ -0,0 +1,25 @@
+﻿using ServerAPIApp.Domain.Entities;
+
+namespace ServerAPIApp.Contracts.Abstractions
+{
+    public interface IProblemVersionLanguageRepository
+    {
+        Task<ProblemVersionLanguage?> GetByIdsAsync
+            (Guid versionId,
+            Guid languageId,
+            CancellationToken cancellationToken = default);
+        Task<List<ProblemVersionLanguage>?> GetAllSupportedAsync
+            (Guid versionId,
+            CancellationToken cancellationToken = default);
+        Task<ProblemVersionLanguage> CreateAsync
+            (ProblemVersionLanguage entity,
+            CancellationToken cancellationToken = default);
+        Task<ProblemVersionLanguage> UpdateAsync
+            (ProblemVersionLanguage entity,
+            CancellationToken cancellationToken = default);
+        Task<ProblemVersionLanguage> DeleteAsync
+           (Guid versionId,
+            Guid languageId,
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ ServerAPIApp.DAL/Contexts/BaseDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ServerAPIApp.Domain.Entities;
+using System.Reflection;
+
+namespace ServerAPIAPP.DAL.Contexts
+{
+    public sealed class BaseDbContext : IdentityDbContext<UserEntity, IdentityRole<Guid>, Guid>
+    {
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options) { }
+
+        public DbSet<LanguageEntity> Languages { get; set; }
+        public DbSet<ProblemEntity> Problems { get; set; }
+        public DbSet<ProblemVersionEntity> ProblemVersions { get; set; }
+        public DbSet<ProblemVersionLanguage> VersionLanguages { get; set; }
+        public DbSet<SubmissionEntity> Submissions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
