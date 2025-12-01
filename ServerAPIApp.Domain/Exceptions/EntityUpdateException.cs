d672dce020220d736cdd65ed130403a2c148a498
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-01 20:19:10 +0300

    [BE] Added new exception classes

---
+++ ServerAPIApp.Domain/Exceptions/EntityUpdateException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAPIApp.Domain.Exceptions
+{
+    public class EntityUpdateException : ApplicationException
+    {
+        public EntityUpdateException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
---
+++ ServerAPIApp.Domain/Exceptions/RoleUpdateException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAPIApp.Domain.Exceptions
+{
+    public class RoleUpdateException : ApplicationException
+    {
+        public RoleUpdateException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
