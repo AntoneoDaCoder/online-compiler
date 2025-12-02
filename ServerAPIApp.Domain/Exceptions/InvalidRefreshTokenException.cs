be5f51927c3d17405f204274058ef2a0206fd1a6
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-02 16:36:10 +0300

    [BE] Created new exception

---
+++ ServerAPIApp.Domain/Exceptions/InvalidRefreshTokenException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAPIApp.Domain.Exceptions
+{
+    public class InvalidRefreshTokenException : ApplicationException
+    {
+        public InvalidRefreshTokenException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
