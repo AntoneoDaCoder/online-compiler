2cdaedbae8eabb72404c313a9f37393a1b01b9aa
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-02 16:37:09 +0300

    [BE] Created refresh token usecase and case handler

---
+++ ServerAPIApp.Core/UseCases/Users/RefreshAccessTokenCase.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAPIApp.Core.UseCases.Users
+{
+    public record RefreshAccessTokenCase(Guid InitiatorId, string AccessToken) : IRequest<string>;
+}
