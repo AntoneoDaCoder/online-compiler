3e84a0c36bb420d3e585569e759f170cf0bc1a42
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-05 20:51:54 +0300

    [BE] Updated extension class: removed all the managers because they in fact perform controller-like logic

--- ServerAPIApp.Core/Extensions/ServiceCollectionExtension.cs
+++ ServerAPIApp.Core/Extensions/ServiceCollectionExtension.cs
@@ -1,10 +1,10 @@
 ﻿using k8s;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using ServerAPIApp.Core.Abstractions;
 using ServerAPIApp.Core.Configs;
-using ServerAPIApp.Core.Repositories;
 using ServerAPIApp.Core.Services;
 using ServerAPIApp.DAL.Extensions;
 using System.Reflection;
@@ -13,79 +13,18 @@ namespace ServerAPIApp.Core.Extensions
 {
     public static class ServiceCollectionExtension
     {
-        private static readonly string[] SupportedLanguages = new[]
-        {
-            "csharp", /*"swift",*/ "java", "postgresql",
-            "mssql", "nodejs", "kotlin", "typescript"
-        };
-
         public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddSingleton<ProblemRepository>();
-
-            services.AddSingleton<IKubernetes>(sp =>
-            {
-                var kubeConfig = KubernetesClientConfiguration.BuildDefaultConfig();
-                return new Kubernetes(kubeConfig);
-            });
-
-            var useComposite = config.GetValue<bool>("UseComposite");
-
-            if (useComposite)
-            {
-                Console.WriteLine("[API] Server starts in composite mode");
-
-                services.Configure<LanguageConfig>("composite", config.GetSection($"Languages:composite"));
-
-                services.AddSingleton<CompositeKubernetesJobManager>(sp =>
-                {
-                    var mgr = new CompositeKubernetesJobManager(
-                        sp.GetRequiredService<IKubernetes>(),
-                        sp.GetRequiredService<IOptionsMonitor<LanguageConfig>>());
-
-                    foreach (var lang in SupportedLanguages)
-                        mgr.RegisterLanguage(lang);
-
-                    return mgr;
-                });
-
-                foreach (var lang in SupportedLanguages)
-                {
-                    services.AddSingleton<IKubernetesJobManager>(sp =>
-                        new CompositeJobManagerProxy(lang, sp.GetRequiredService<CompositeKubernetesJobManager>()));
-                }
-            }
-            else
-            {
-                Console.WriteLine("[API] Server starts in default mode");
-
-                foreach (var lang in SupportedLanguages)
-                {
-                    services.Configure<LanguageConfig>(lang, config.GetSection($"Languages:{lang}"));
-                }
-
-                foreach (var lang in SupportedLanguages)
-                {
-                    services.AddSingleton<IKubernetesJobManager>(sp =>
-                    {
-                        var monitor = sp.GetRequiredService<IOptionsMonitor<LanguageConfig>>();
-                        return new KubernetesJobManager(
-                            lang,
-                            sp.GetRequiredService<IKubernetes>(),
-                            monitor
-                        );
-                    });
-                }
-            }
-
-            services.AddHostedService<ManagerAdapter>();
-            services.AddSingleton<ICodeDispatcher, CodeDispatcher>();
-            services.AddHostedService(provider => provider.GetRequiredService<ICodeDispatcher>());
-
             services.ConfigureDbContext();
             services.ConfigureObjectStorage();
             services.ConfigureRepositories();
 
+            //TODO:
+            // add google auth service registration here
+            //
+
+            services.AddDataProtection().SetApplicationName("ServerAPIApp");
+            services.AddSingleton<ISecretProtector, DataProtectionSecretProtector>();
             services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
             services.AddScoped<IJwtTokenService, JwtTokenService>();
 
