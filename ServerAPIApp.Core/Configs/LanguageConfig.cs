2552679377d3e9c33815ca01e5f875dc34842daa
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-05 20:54:34 +0300

    [BE] Moved these classes and interfaces to web api project

--- ServerAPIApp.Core/Configs/LanguageConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerAPIApp.Core.Configs
-{
-    public class LanguageConfig
-    {
-        public string ImageName { get; set; } = string.Empty;
-        public int MemoryLimitMib { get; set; }
-        public int MemoryBudgetMib { get; set; }
-    }
-}
