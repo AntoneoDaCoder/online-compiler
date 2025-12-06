4c44975abdc8fb31b418aaa01f2fdc44bb43c53a
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-06 18:27:24 +0300

    [BE] Created new helper classes to handle code execution data

---
+++ Runners/Runners.Shared/CompilationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Runners.Shared
+{
+    public class CompilationResult
+    {
+        public bool Success { get; set; }
+        public string? CompilationErrors { get; set; }
+        public int TotalTests { get; set; }
+    }
+}
---
+++ Runners/Runners.Shared/ExecutionData.cs
@@ -0,0 +1,13 @@
+﻿namespace Runners.Shared
+{
+    public class ExecutionData
+    {
+        public Guid RequestId { get; set; }
+        public Guid VersionId { get; set; }
+        public Guid UserId { get; set; }
+        public string UserSolution { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public DateTimeOffset RequestDate { get; set; }
+        public int TotalTests { get; set; }
+    }
+}
