6bba447629f3eb951f7f8ef2f198c42253d9516b
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-06 18:50:15 +0300

    [BE] Added jwt configuration code block

--- ServerAPIApp.Core/Extensions/ServiceCollectionExtension.cs
+++ ServerAPIApp.Core/Extensions/ServiceCollectionExtension.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.DataProtection;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using ServerAPIApp.Core.Abstractions;
+using ServerAPIApp.Core.Configs;
 using ServerAPIApp.Core.Services;
 using ServerAPIApp.DAL.Extensions;
 using System.Reflection;
+using System.Security.Claims;
+using System.Text;
 
 namespace ServerAPIApp.Core.Extensions
 {
@@ -19,6 +24,34 @@ namespace ServerAPIApp.Core.Extensions
             //TODO:
             // add google auth service registration here
             //
+
+            var jwtSection = config.GetSection("JwtSettings");
+            var jwtSettings = jwtSection.Get<JwtSettings>();
+
+            services.Configure<JwtSettings>(jwtSection);
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                                                Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")!)
+                                                ),
+                    ValidateLifetime = true,
+                    ValidIssuer = jwtSettings.ValidIssuer,
+                    ValidAudience = jwtSettings.ValidAudience,
+                    RoleClaimType = ClaimTypes.Role,
+                };
+            });
 
             services.AddDataProtection().SetApplicationName("ServerAPIApp");
             services.AddSingleton<ISecretProtector, DataProtectionSecretProtector>();
