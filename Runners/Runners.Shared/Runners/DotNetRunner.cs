1a4b3af937e7595b7e20eda62b4818eb2386ae92
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-06 20:47:01 +0300

    [BE] updated dotnet runner

--- Runners/Runners.Shared/Runners/DotNetRunner.cs
+++ Runners/Runners.Shared/Runners/DotNetRunner.cs
@@ -9,7 +9,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-using ServerAPIApp.Domain.Exceptions.UnauthorizedExceptions;
+using ServerAPIApp.Domain.Exceptions.BadRequestExceptions;
 
 namespace Runners.Shared.Runners
 {
