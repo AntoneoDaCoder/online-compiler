8a3ddaf9720e032d68a46bdf7858fa301d5972e3
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-11 02:16:20 +0300

    [Refactoring/BE] Removed unused classes

--- Runners/KotlinRunner/src/main/kotlin/dto/Problem.kt
+++
@@ -1,9 +0,0 @@
-package dto
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Problem(
-    val name: String = "",
-    val additionalDefinitions: MutableList<AdditionalDefinition> = mutableListOf(),
-    val testCases: MutableList<TestCase> = mutableListOf()
-)
