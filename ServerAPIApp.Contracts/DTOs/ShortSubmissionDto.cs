038e2ff1af6ba2d30b1215e1c7953062dbcbe1b1
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-11 02:47:28 +0300

    [BE] created new dto types

---
+++ ServerAPIApp.Contracts/DTOs/ShortSubmissionDto.cs
@@ -0,0 +1,17 @@
+﻿using ServerAPIApp.Domain.Entities;
+
+namespace ServerAPIApp.Contracts.DTOs
+{
+    public record ShortSubmissionDto(Guid Id, string SolutionLanguage, int PassedTests, int TotalTests)
+    {
+        public static ShortSubmissionDto From(Guid id, string solutionLanguage, int passedTests, int totalTests)
+        {
+            return new ShortSubmissionDto(id, solutionLanguage, passedTests, totalTests);
+        }
+
+        public static ShortSubmissionDto From(SubmissionEntity entity)
+        {
+            return new ShortSubmissionDto(entity.Id, entity.SolutionLanguage, entity.PassedTests, entity.TotalTests);
+        }
+    }
+}
---
+++ ServerAPIApp.Contracts/DTOs/SubmissionDto.cs
@@ -0,0 +1,17 @@
+﻿using ServerAPIApp.Domain.Entities;
+
+namespace ServerAPIApp.Contracts.DTOs
+{
+    public record SubmissionDto(Guid SubmissionId, Guid VersionId, string ProblemSlug, string SolutionLanguage, string Solution, int NumPassedTests, int TotalTests)
+    {
+        public static SubmissionDto From(Guid subId, Guid verId, string pSlug, string sLang, string solution, int numPassed, int numTotal)
+        {
+            return new SubmissionDto(subId, verId, pSlug, sLang, solution, numPassed, numTotal);
+        }
+
+        public static SubmissionDto From(SubmissionEntity entity)
+        {
+            return new SubmissionDto(entity.Id, entity.ProblemVersionId, entity.ProblemVersion.Problem.Slug, entity.SolutionLanguage, entity.Solution, entity.PassedTests, entity.TotalTests);
+        }
+    }
+}
