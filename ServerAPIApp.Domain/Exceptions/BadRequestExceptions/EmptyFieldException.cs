2176c8314a2328183747ff87aa7f8e2003564344
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-11 02:48:18 +0300

    [BE] created new exception

---
+++ ServerAPIApp.Domain/Exceptions/BadRequestExceptions/EmptyFieldException.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerAPIApp.Domain.Exceptions.BadRequestExceptions
+{
+    public class EmptyFieldException : BadRequestException
+    {
+        public EmptyFieldException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
