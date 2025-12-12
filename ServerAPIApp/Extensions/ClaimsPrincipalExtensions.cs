4de7546a3fbba88d69f51ef69e17173ea35cb12a
Anton Urbanovich <antonurbanovic@gmail.com>
2025-12-12 19:12:59 +0300

    [BE] created new simple qol enhancer class

---
+++ ServerAPIApp/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace ServerAPIApp.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static IEnumerable<string> GetRoles(this ClaimsPrincipal user)
+        {
+            return user.Claims.Where(claim => claim.Type == ClaimTypes.Role).Select(c => c.Value).ToList();
+        }
+    }
+}
